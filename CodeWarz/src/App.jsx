aa970f90c8ebf91c1a719fa846b26c4e4f25109c
hansha007 <hanshamalik07@gmail.com>
2026-04-26 15:28:33 +0530

    step 0:Setup complete with routing and navigation

--- CodeWarz/src/App.jsx
+++ CodeWarz/src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Problems from "./pages/Problems";
+
+function Home() {
+  return (
+    <>
+      <h1>CodeWarz ⚔️</h1>
+      <Link to="/problems">Go to Problems</Link>
+    </>
+  );
+}
+
 function App() {
-  return <h1>CodeWarz </h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/problems" element={<Problems />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
